223a9e1a9f456d1bab7ae89239c3646f942eb05c
uki <wijekoonuthpala123@gmail.com>
2026-02-05 11:11:10 +0530

    add next confix to the image type

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // <-- Added for Google user images
+      },
     ],
   },
   async headers() {
